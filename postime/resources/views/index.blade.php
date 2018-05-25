2fc19ee339cb9aaeb09d89897b4a89bdf4e19ae1
szkaym <good.to.g.o.on@live.jp>
2018-05-25 13:48:24 +0900

    mod html

---
+++ postime/resources/views/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.default')
+
+@section('content')
+<div class="wrapper">
+    <h1 id="logo"><span class="adjuster">P</span></h1>
+
+    <div id="login-form">
+        {{Form::open(['url'=>'/'])}}
+        
+            <div class="form-item">
+                <label class="form-label">your email</label>
+                <span class="form-input">{{Form::input('text', 'user_email')}}</span>
+            </div>
+            
+            <div class="form-item">
+                <label class="form-label">password</label>
+                <span class="form-input">{{Form::input('password', 'user_password')}}</span>
+            </div>
+
+            <div class="form-controlls">
+                {{Form::submit('Signin')}}
+            </div>
+
+        {{Form::close()}}
+    </div>
+
+</div>
+@endsection
