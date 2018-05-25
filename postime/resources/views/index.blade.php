fa5417ea92821cb10ab9395ca2d8922f49c582c9
szkaym <good.to.g.o.on@live.jp>
2018-05-25 16:12:27 +0900

    mod css

--- postime/resources/views/index.blade.php
+++ postime/resources/views/index.blade.php
@@ -16,6 +16,13 @@
                 <label class="form-label">password</label>
                 <span class="form-input">{{Form::input('password', 'user_password')}}</span>
             </div>
+            
+            <div class="form-item">
+                <label class="form-label">
+                    {{Form::input('checkbox', 'remember')}}
+                    <span class="form-label-tape">remember me</span>
+                </label>
+            </div>
 
             <div class="form-controlls">
                 {{Form::submit('Signin')}}
