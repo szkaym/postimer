21013928e7a36ac4924fec7e18962c4fb17456a2
SuzukiAyumu <good.to.g.o.on@live.jp>
2018-05-27 00:04:44 +0900

    add index

--- postime/resources/views/index.blade.php
+++ postime/resources/views/index.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
 <div class="wrapper">
-    <h1 id="logo"><span class="adjuster">P</span></h1>
+    <!-- <h1 id="logo"><span class="adjuster">P</span></h1> -->
+
+    <h1 id="site-title">post it your work timestamp</h1>
 
     <div id="login-form">
         {{Form::open(['url'=>'/'])}}
@@ -20,15 +22,33 @@
             <div class="form-item">
                 <label class="form-label">
                     {{Form::input('checkbox', 'remember')}}
-                    <span class="form-label-tape">remember me</span>
+                    <span class="form-label-tape">save login status</span>
                 </label>
             </div>
 
             <div class="form-controlls">
                 {{Form::submit('Signin')}}
+
+                <div class="foget-password">
+                    <a href="">foget password?</a>
+                </div>
             </div>
 
         {{Form::close()}}
+    </div>
+
+    <div id="index-footer">
+        <p class="say-other">What is this ?</p>
+        <p class="say-postime">This is a mini blog for your working time records.</p>
+        <p class="say-other">How to do a registration?</p>
+        <p class="say-postime">see <a href=''>here</a>.</p>
+        <p class="say-other">Does it take time ?</p>
+        <p class="say-postime">A little. Sorry.</p>
+        <p class="say-other">A free ?</p>
+        <p class="say-postime">Of course!</p>
+        <p class="say-other">Why collection at my working timestamp ?</p>
+        <p class="say-postime">We study and learning of AI.<br>
+            we feedback to you the new feature, new services, graph of your post data ... etc.</p>
     </div>
 
 </div>
--- postime/resources/views/layouts/default.blade.php
+++ postime/resources/views/layouts/default.blade.php
@@ -15,7 +15,7 @@
     <div id="app">
 @yield('content')
     </div>
-
+    
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
