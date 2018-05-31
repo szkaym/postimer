13ba7d642a7499c25ca385674e92f89a00a5d984
SuzukiAyumu <good.to.g.o.on@live.jp>
2018-05-31 22:52:12 +0900

    mod migration

--- postime/database/migrations/2018_05_23_145627_create_t_users_table.php
+++ postime/database/migrations/2018_05_23_145627_create_t_users_table.php
@@ -17,16 +17,15 @@ class CreateTUsersTable extends Migration
             $table->increments('id');
             $table->string('user_code', 32);
             $table->string('user_name', 191);
-            $table->unsignedInteger('role_id');
             $table->string('user_email', 191);
             $table->string('user_password', 100);
-            $table->rememberToken();
             $table->boolean('user_active')->default(0);
-            $table->boolean('user_deleted')->default(0);
+            $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
 
-            $table->unique(['user_code', 'user_deleted'], 'unique_user_code');
-            $table->unique(['user_email', 'user_deleted'], 'unique_user_email');
+            $table->index('user_code');
+            $table->index('user_email');
         });
     }
 
